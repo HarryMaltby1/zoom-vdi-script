d252daea1bd418b11ab4d46fbc538b025a1a0704
HarryMaltby1 <72517479+HarryMaltby1@users.noreply.github.com>
2025-07-04 15:25:48 +0100

    Update Install-ZoomVDI.ps1

--- Install-ZoomVDI.ps1
+++ Install-ZoomVDI.ps1
@@ -5,8 +5,8 @@ Invoke-WebRequest -Uri "https://zoom.us/download/vdi/6.4.11.26350/ZoomInstallerV
 Invoke-WebRequest -Uri "https://zoom.us/download/vdi/6.4.11.26350/ZoomVDIUniversalPluginx64.msi?archType=x64" -OutFile "ZoomAllInOnePlugin.msi"
 
 # Install Universal Plugin
-Start-Process -FilePath "msiexec.exe" -ArgumentList @("/i", "ZoomAllInOnePlugin.msi", "INSTALLFORAVD=1", "/qn", "/log", "C:\Temp\ZoomPluginInstall.log") -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList @("/i", "ZoomAllInOnePlugin.msi", "INSTALLFORAVD=1", "/qn", "/log", "C:\Temp\ZoomPluginInstall.log") 
 
 # Install Zoom VDI with config
-Start-Process msiexec.exe -ArgumentList '/i "ZoomInstallerVDI.msi" /qn ZConfig="kCmdParam_InstallOption=8;zSSOHost=plymouth.zoom.us"' -Wait
+Start-Process msiexec.exe -ArgumentList '/i "ZoomInstallerVDI.msi" /qn ZConfig="kCmdParam_InstallOption=8;zSSOHost=plymouth.zoom.us"' 
 
