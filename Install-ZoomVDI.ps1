b65589a54fd105c3bc4f995051df700a1f9dfcf4
HarryMaltby1 <72517479+HarryMaltby1@users.noreply.github.com>
2025-07-04 11:06:37 +0100

    Create Install-ZoomVDI.ps1

---
+++ Install-ZoomVDI.ps1
@@ -0,0 +1,11 @@
+# Download Zoom VDI Installer
+Invoke-WebRequest -Uri "https://zoom.us/download/vdi/6.4.11.26350/ZoomInstallerVDI.msi?archType=x64" -OutFile "ZoomInstallerVDI.msi"
+
+# Download Zoom Universal Plugin
+Invoke-WebRequest -Uri "https://zoom.us/download/vdi/6.4.11.26350/ZoomVDIUniversalPluginx64.msi?archType=x64" -OutFile "ZoomAllInOnePlugin.msi"
+
+# Install Universal Plugin
+Start-Process msiexec.exe -ArgumentList "/i ZoomAllInOnePlugin.msi INSTALLFORAVD=1 /qn" -Wait
+
+# Install Zoom VDI with config
+Start-Process msiexec.exe -ArgumentList "/i ZoomInstallerVDI.msi /qn /ZConfig=`"kCmdParam_InstallOption=8;zSSOHost=plymouth.zoom.us`"" -Wait
